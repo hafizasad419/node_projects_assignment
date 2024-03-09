b294f5256c6b5bfb899ad253df1c450dd68bd911
hafizasad419 <hafizasad419@gmail.com>
2024-03-09 23:31:01 +0500

    add exercise_01 file for checking purpose

---
+++ src/exercise_01.ts
@@ -0,0 +1,3 @@
+// I have already setup nodeJS, Typescript and VS Code.
+
+// That's Why I am able to code here in VS code with Typescript and I am able to run JS outside of browser.(Thanks to nodeJS)😁
